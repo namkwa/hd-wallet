a5ebcce99863db3ad50e72c18b2c88ce77dbbe18
namkwa <h.houis1507@gmail.com>
2023-03-15 01:54:12 +0100

    connection to tpm

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::str::FromStr;
+
 fn main() {
-    println!("Hello, world!");
+    use tss_esapi::{Context, TctiNameConf};
+
+    // Specify the TCTI (TPM Command Transmission Interface) name for the TPM device
+    let tcti_name = "device:/dev/tpmrm0";
+
+    // Create a new TPM context using the specified TCTI
+    let tcti = TctiNameConf::from_str(tcti_name).unwrap();
+    let _context = Context::new(tcti).unwrap();
 }
