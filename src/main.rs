5b5414b90319ad4a246c00adbbcd2410236d343b
namkwa <h.houis1507@gmail.com>
2023-04-13 22:18:44 +0200

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,35 @@
 use std::str::FromStr;
 
+use hmac::{Hmac, Mac};
+use sha2::Sha256;
+
 fn main() {
     use tss_esapi::{Context, TctiNameConf};
 
-    // Specify the TCTI (TPM Command Transmission Interface) name for the TPM device
+    //connection to the TPM
     let tcti_name = "device:/dev/tpmrm0";
 
-    // Create a new TPM context using the specified TCTI
     let tcti = TctiNameConf::from_str(tcti_name).unwrap();
-    let _context = Context::new(tcti).unwrap();
+    let mut context = Context::new(tcti).unwrap();
+
+    //generation of a subkey from a master key using HKDF
+    type HmacSha256 = Hmac<Sha256>;
+    let master_key = [0x01; 32];
+    let salt = [0x02; 16];
+
+    let mut hmac = HmacSha256::new_from_slice(&master_key).unwrap();
+
+    hmac.update(&salt);
+
+    let prk = hmac.finalize().into_bytes();
+
+    let info = [0x03; 8];
+    let subkey_len: i32 = 16;
+
+    let mut hmac = Hmac::<Sha256>::new_from_slice(&prk).unwrap();
+
+    hmac.update(&info);
+    hmac.update(&[1u8]);
+    hmac.update(&subkey_len.to_be_bytes());
+    let subkey = hmac.finalize().into_bytes();
 }
